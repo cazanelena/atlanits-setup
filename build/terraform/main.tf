52fc079eb1fe2714c4d6c1e6286d875df9960bf1
Elena Cazan <cazan.elena23@yahoo.com>
2024-03-22 11:10:17 +0000

    added some directories and files

---
+++ build/terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "terraform-state-atlantis435"
+    key    = "atlantis-project"
+    region = "eu-west-2"
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
