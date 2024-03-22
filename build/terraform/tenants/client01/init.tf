3599fe8a689e357b3bcbd388b0dbf0a535e99e28
Elena Cazan <cazan.elena23@yahoo.com>
2024-03-22 16:53:04 +0000

    delete ec2 instance

--- build/terraform/tenants/client01/init.tf
+++ build/terraform/tenants/client01/init.tf
@@ -9,15 +9,3 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "InstanceClient01-Testing"
-  }
-}
